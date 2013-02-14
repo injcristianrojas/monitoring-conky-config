c7ba1f12f63c1627d4d71cc1d27e0677a598f625
Cristián Rojas <injcristianrojas@gmail.com>
2013-02-14 18:31:12 -0300

    Indentation change, from 2 to 4 spaces

--- scripts/service_defs_example.py
+++ scripts/service_defs_example.py
@@ -22,19 +22,19 @@ TYPE_NAGIOS = 0
 TYPE_UP_DOWN = 1
 
 service_definitions = {
-  'mon1': {
-    'command': 'ls -la',
-    'name': 'Check ls',
-    'type': TYPE_UP_DOWN
-  },
-  'mon2': {
-    'command': 'python -c "import sys;from random import randint;sys.exit(randint(0,2))"',
-    'name': 'Random Nagios exit code',
-    'type': TYPE_NAGIOS
-  },
-  'mon3': {
-    'command': 'python -c "import sys;from random import randint;sys.exit(randint(0,2))"',
-    'name': 'Random UP/DOWN exit code',
-    'type': TYPE_UP_DOWN
-  },
+    'mon1': {
+        'command': 'ls -la',
+        'name': 'Check ls',
+        'type': TYPE_UP_DOWN
+    },
+    'mon2': {
+        'command': 'python -c "import sys;from random import randint;sys.exit(randint(0,2))"',
+        'name': 'Random Nagios exit code',
+        'type': TYPE_NAGIOS
+    },
+    'mon3': {
+        'command': 'python -c "import sys;from random import randint;sys.exit(randint(0,2))"',
+        'name': 'Random UP/DOWN exit code',
+        'type': TYPE_UP_DOWN
+    },
 }
